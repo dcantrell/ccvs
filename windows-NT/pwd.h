0dd03ffebbcc8181758cb74f2a0c7cbd89ac847f
cvs2hg <cvs2hg@localhost>
1995-08-04 05:03:55 +0000

    fixup commit for branch 'wnt_port_branch'

--- windows-NT/pwd.h
+++
@@ -1,4 +0,0 @@
-/* pwd.h --- Windows NT version of the password library routine header file
-   Jim Blandy <jimb@cyclic.com> --- July 1995  */
-
-/* nothing to put here yet.  We'll see what we need.  */
