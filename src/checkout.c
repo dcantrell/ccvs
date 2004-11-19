bb72bd833e4b352a37169a534c514e0cb49d2e92
mdb <mdb@localhost>
2004-11-19 07:23:57 +0000

    * checkout.c (checkout_proc): Passing the repository to tag_check_valid seems to stop the assertion failure in recurse.c do_recursion. * sanity.sh (basic2-21a): Removed. (basic2-21b): Fixed.

--- src/checkout.c
+++ src/checkout.c
@@ -1053,7 +1053,8 @@ internal error: %s doesn't start with %s in checkout_proc",
 	which = W_REPOS;
 	if (tag != NULL && !tag_validated)
 	{
-	    tag_check_valid (tag, argc - 1, argv + 1, 0, aflag, NULL);
+	    tag_check_valid (tag, argc - 1, argv + 1, 0, aflag,
+			     repository);
 	    tag_validated = 1;
 	}
     }
