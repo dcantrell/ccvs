e999c4eb8e8f639f0bdc3d5141f098b5fd6541ba
dprice <dprice@localhost>
2006-02-27 17:04:57 +0000

    * lock.c (internal_lock): Back out previous change, we don't change user visible output on stable unless absolutely necessary.

--- src/lock.c
+++ src/lock.c
@@ -1083,9 +1083,8 @@ internal_lock (lock, xrepository)
     if (set_lock (lock, 1) != L_OK)
     {
 	if (!really_quiet)
-	    error (0, 0,
-		   "failed to obtain lock `%s' in repository directory `%s'.",
-		   lock->lockdirname, Short_Repository (lock->repository));
+	    error (0, 0, "failed to obtain history lock in repository `%s'",
+		   xrepository);
 
 	return 0;
     }
